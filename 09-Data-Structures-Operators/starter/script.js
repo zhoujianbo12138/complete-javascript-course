db6fd28e0427df0242c6e08c1e3acab0817a8ad7
zhoujianbo <z2949426236@gmail.com>
2022-10-26 00:02:32 +0800

    modified:   09-Data-Structures-Operators/starter/script.js

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -161,7 +161,62 @@ const game = {
     team2: 6.5,
   },
 };
+//1 Create one player array for each team (variables 'players1' and 'players2')
 
-// const player1 = [...game.players[0]];
-// const player2 = [...game.players[1]];
+// const [goalkeeper1, ...fieldPlayers1] = [...game.players[0]];
+// const player1 = [goalkeeper1, fieldPlayers1];
 // console.log(player1);
+// const [goalkeeper2, ...fieldPlayers2] = [...game.players[1]];
+// const player2 = [goalkeeper2, fieldPlayers2];
+// console.log(player2);
+// //2. The first player in any player array is the goalkeeper and the others are field players.
+// //For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name,
+// // and one array ('fieldPlayers') with all the remaining 10 field players
+// const teams1 = {
+//   gk: goalkeeper1,
+//   fieldPlayers: fieldPlayers1,
+// };
+// // 3. Create an array 'allPlayers' containing all players of both teams (22 players)
+// const allPlayers = [...game.players[0], ...game.players[1]];
+// console.log(allPlayers);
+// //4. During the game, Bayern Munich (team 1) used 3 substitute players.
+// //So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+// const players1Final = [...game.players[0], 'Thiago', 'Coutinho', 'Perisic'];
+// console.log(players1Final);
+// //5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+// const {
+//   odds: { team1: team1, x: draw, team2: team2 },
+// } = game;
+// console.log(team1, draw, team2);
+// //6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array)
+// //and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+// const occrCount = function (element, arr) {
+//   let count = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (element === arr[i]) {
+//       count += 1;
+//     }
+//   }
+//   return count;
+// };
+
+// let goal = [];
+// const scored = [...game.scored];
+// for (let i = 0; i < allPlayers.length; i++) {
+//   goal[i] = occrCount(allPlayers[i], scored);
+// }
+
+// const printGoals = function (...names) {
+//   let str = '';
+//   for (let i = 0; i < names.length; i++) {
+//     let index = allPlayers.indexOf(names[i]);
+//     str += `${names[i]}: ${goal[index]}`;
+//   }
+//   return str;
+// };
+// console.log(printGoals('Lewandowski'));
+// //7. The team with the lower odd is more likely to win.
+// // Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+// let winer = (team1 < team2 && 'team1') || 'team2';
+// console.log(winer);
